82edfea522f05e230db93b1836769bdc4fda56b8
Anmol walia <anmolwalia025@gmail.com>
2025-06-16 21:00:01 +0530

    razorpay_

--- apps/user-app/app/loading.tsx
+++ apps/user-app/app/loading.tsx
@@ -1,11 +1,7 @@
-
-function loading() {
+export default function loading() {
   return (
-<div className="w-full h-screen flex justify-center items-center">
-        <div className="w-12 h-12 rounded-full border-b-2 border-gray-500 animate-spin "></div>
-        
-</div>
-)
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-25">
+      <div className="w-12 h-12 rounded-full border-b-2 border-gray-600 animate-spin"></div>
+    </div>
+  );
 }
-
-export default loading

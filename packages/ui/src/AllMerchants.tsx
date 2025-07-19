684fceb84e11de5c769bd0dbd3e830f8ea6cee99
Anmol walia <anmolwalia025@gmail.com>
2025-07-19 22:49:57 +0530

    userManagement

--- packages/ui/src/AllMerchants.tsx
+++ packages/ui/src/AllMerchants.tsx
@@ -165,7 +165,7 @@ export default function AllMerchants({Merchants}:{Merchants:any[]}) {
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">User</th>
+                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Merchant</th>
                   <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                   <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Wallet Balance</th>
                   <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Joined</th>
